0e885a5bb6e46a3c5f84327f402e74c7506f6801
Andrew Edgar <andrew@andrewedgar.io>
2025-06-05 21:55:18 -0400

    Dicked around

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchURL(String urlString) async {
   final Uri url = Uri.parse(urlString);
-  if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-    debugPrint('Could not launch $urlString');
-  }
+  await launchUrl(url, mode: LaunchMode.externalApplication);
 }
